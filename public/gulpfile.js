cd4e075f731515d7f84cacfea3ec30820aec282b
AxMari <AlexisMarien@gmail.com>
2016-05-19 17:04:20 -0400

    browser-sync

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
-var browserSync = require("browser-sync").create();
+var jshint = require('gulp-jshint');
+var browserSync = require('browser-sync').create;
+
+gulp.task('jshint', function() {
+	return gulp.src(['./public/controllers/*.js', './public/script.js'])
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'));
+});
 
 gulp.task('browser-sync', function() {
 	    browserSync.init({
@@ -8,7 +15,6 @@ gulp.task('browser-sync', function() {
 	        }
 
 	    });
-	gulp.watch(["*.html","*.css","*.js"])
+	gulp.watch(["*.html","*.css","*.js", "*.styl"])
 		.on('change',browserSync.reload);
 	});	
-
