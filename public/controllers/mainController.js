c9ec79119183160a57ae7a1e7bbf5933f715fd6c
Britta Rodenbeck <rodenbeck.britta@gmail.com>
2016-05-22 15:43:37 -0400

    started working on entrance to logic

--- public/controllers/mainController.js
+++ public/controllers/mainController.js
@@ -1,4 +1,18 @@
 angular.module('mapModule')
 	.controller('mainController', function($scope){
+		$scope.arts = function() {
+			// code
+		}
 
+		$scope.beer = function() {
+			// code
+		}
+
+		$scope.coffee = function() {
+			// code
+		}
+
+		$scope.shopping = function() {
+			// code
+		}
 	});
