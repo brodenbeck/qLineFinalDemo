2f01ccb18ae7172bd84a6521a5027e3e2d1ec0aa
AxMari <AlexisMarien@gmail.com>
2016-05-24 18:17:29 -0400

    scroller functionality complete

--- controllers/railController.js
+++ controllers/railController.js
@@ -17,6 +17,15 @@ angular.module('mapModule')
         }
         console.log(returnedArr.length + '/' + $scope.place_index);
     };
+     $scope.back = function () {
+        if ($scope.place_index <= returnedArr.length - 1 &&
+        	$scope.place_index > 0) {
+            	$scope.place_index --;
+        } else {
+        	$scope.place_index = returnedArr.length - 1;
+        }
+        console.log(returnedArr.length + '/' + $scope.place_index);
+    };
 
 });
 
