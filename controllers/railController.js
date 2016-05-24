c89cfa6dea5a8957873a56a648394e61cd9fc8b7
Britta Rodenbeck <rodenbeck.britta@gmail.com>
2016-05-24 14:34:53 -0400

    fixed issue where array always returned empty

--- controllers/railController.js
+++ controllers/railController.js
@@ -1,7 +1,7 @@
 angular.module('mapModule')
 	.controller('railController', function($scope, railService){
 		var returnedArr = railService.getData();
-		if(returnedArr = []) {
+		if(returnedArr.length === 0) {
 			$scope.yelpArr = [{ phone: "I'm sorry, there is nothing available for your interests at this stop."}];
 		} else {
 			$scope.yelpArr = returnedArr;
