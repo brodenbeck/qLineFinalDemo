a7abd6dc7089134bf9d546a64f4c5f5ad2634b15
AxMari <AlexisMarien@gmail.com>
2016-05-24 17:21:16 -0400

    scroll done

--- controllers/railController.js
+++ controllers/railController.js
@@ -6,4 +6,28 @@ angular.module('mapModule')
 		} else {
 			$scope.yelpArr = returnedArr;
 		}
-	});
+
+    $scope.place_index = 0;
+
+    $scope.next = function () {
+        if ($scope.place_index >= returnedArr.length - 1) {
+            $scope.place_index = 0;
+        } else {
+            $scope.place_index++;
+        }
+        console.log(returnedArr.length + '/' + $scope.place_index);
+    };
+
+});
+
+
+	// 	$scope.forwardcounter = function(){
+	// 		console.log(returnedArr[0]);
+	// 			return returnedArr[0];
+	// 		};
+	// 	$scope.backwardscounter = function(){
+	// 		console.log(returnedArr[1]);
+	// 			return returnedArr[1];
+	// 		};
+
+	// });
